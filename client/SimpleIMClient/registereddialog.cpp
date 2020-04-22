25aca4dbbc424a6b355d1f1ba0867b46ecb09c32
sylong <853949802@qq.com>
2020-04-23 01:36:57 +0800

    commit

--- client/SimpleIMClient/registereddialog.cpp
+++ client/SimpleIMClient/registereddialog.cpp
@@ -19,10 +19,25 @@ void registeredDialog::on_pushButton_clicked()
     if(sc->isconnetion)
     {
         QString sendMessage = ui->lineEdit->text();
-        if(!sendMessage.isEmpty())
+        QString sendMessage1 = ui->lineEdit_2->text();
+        QString sendMessage2 = ui->lineEdit_3->text();
+        QString sendMessage3 = ui->lineEdit_4->text();
+
+        if(!sendMessage.isEmpty()&&!sendMessage1.isEmpty()&&!sendMessage2.isEmpty()&&!sendMessage3.isEmpty())
         {
-            sendMessage+='\n';
-            sc->TCP_sendMesSocket->write(sendMessage.toUtf8());
+            if(!(sendMessage1!=sendMessage2))
+            {
+                sendMessage+='\n';
+                sc->TCP_sendMesSocket->write(sendMessage.toUtf8());
+                sendMessage1+='\n';
+                sc->TCP_sendMesSocket->write(sendMessage.toUtf8());
+                sendMessage2+='\n';
+                sc->TCP_sendMesSocket->write(sendMessage.toUtf8());
+                sendMessage3+='\n';
+                sc->TCP_sendMesSocket->write(sendMessage.toUtf8());
+            }
+            else
+                QMessageBox::warning(this,"错误","两次密码不一致!",QMessageBox::Ok);
         }
         else
             QMessageBox::warning(this,"错误","消息不能为空!",QMessageBox::Ok);
